40ca9dffb9c26f9642bcb7791784ada67cbf81bd
erik.telepovsky <erik.telepovsky@49924331-b1e2-4916-88b7-9d944329b4f6>
2011-03-17 09:00:37 +0000

    enter poll ID -> hint & input type
    git-svn-id: http://dossee.atlantis.fw.sk/droids@16 49924331-b1e2-4916-88b7-9d944329b4f6

--- openarsandroidc/src/dossee/droids/openarsActivity.java
+++ openarsandroidc/src/dossee/droids/openarsActivity.java
@@ -1,7 +1,14 @@
 package dossee.droids;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class openarsActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +16,6 @@ public class openarsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+   
     }
 }
