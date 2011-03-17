cc353c629e759d4a47ef9d4323756eb316fb7d85
vera.fallman <vera.fallman@49924331-b1e2-4916-88b7-9d944329b4f6>
2011-03-17 10:05:38 +0000

    git-svn-id: http://dossee.atlantis.fw.sk/droids@19 49924331-b1e2-4916-88b7-9d944329b4f6

--- openarsandroidc/src/dossee/droids/openarsActivity.java
+++ openarsandroidc/src/dossee/droids/openarsActivity.java
@@ -14,13 +14,35 @@ public class openarsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.main);
-        setContentView(R.layout.answers_ch);
+       // setContentView(R.layout.answers_ch);
+        setContentView(R.layout.answers_rb);
+        /**
+         * Question list
+         * TODO: fetch the questions from JSON here 
+         * instead of lv_answers
+         */
         ListView lView = (ListView)findViewById(R.id.lv_questions);
+
+        /**
+         * make a multiple choice answer layout
+         */
+        /*
+        
         lView.setAdapter(new ArrayAdapter<String>(this,
         			android.R.layout.simple_list_item_multiple_choice,lv_items));
         lView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        */
+        
+        /**
+         * make a single choice answer layout
+         */
+        
+        lView.setAdapter(new ArrayAdapter<String>(this,
+    			android.R.layout.simple_list_item_single_choice,lv_items));
+        lView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
     }
     
+         
    
   
 }
