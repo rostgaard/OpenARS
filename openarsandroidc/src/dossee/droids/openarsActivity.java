8aaa6f9d093cde643d8e7368110a471d75ac83b2
vera.fallman <vera.fallman@49924331-b1e2-4916-88b7-9d944329b4f6>
2011-03-17 09:28:28 +0000

    Test version of a multiple choice question
    git-svn-id: http://dossee.atlantis.fw.sk/droids@17 49924331-b1e2-4916-88b7-9d944329b4f6

--- openarsandroidc/src/dossee/droids/openarsActivity.java
+++ openarsandroidc/src/dossee/droids/openarsActivity.java
@@ -1,21 +1,30 @@
 package dossee.droids;
-import android.app.Activity;
-import android.content.Intent;
+
+import android.app.*;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.*;
+
 
 public class openarsActivity extends Activity {
     /** Called when the activity is first created. */
+	
+	private String lv_items[] = {"First question", "Second question"};
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-   
+       // setContentView(R.layout.main);
+        setContentView(R.layout.answers_ch);
+        ListView lView = (ListView)findViewById(R.id.lv_questions);
+        lView.setAdapter(new ArrayAdapter<String>(this,
+        			android.R.layout.simple_list_item_multiple_choice,lv_items));
+        lView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
+    
+   
+  
 }
+
+
+
+
