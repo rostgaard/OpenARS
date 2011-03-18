5be832c8fa6a88efade37c96d8e2ce94ed8fa250
marcos.sanchez <marcos.sanchez@49924331-b1e2-4916-88b7-9d944329b4f6>
2011-03-18 08:43:16 +0000

    git-svn-id: http://dossee.atlantis.fw.sk/droids@36 49924331-b1e2-4916-88b7-9d944329b4f6

--- openarswebc/app/controllers/Application.java
+++ openarswebc/app/controllers/Application.java
@@ -10,6 +10,19 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
+    	String name = "Droids";
+        render(name);
+    }
+    
+    public static void makenewpoll() {
+        render();
+    }
+    
+    public static void joinpoll(String id) {
+        render(id);
+    }
+    
+    public static void managepoll() {
         render();
     }
 
