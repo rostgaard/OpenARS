0ec1ddb492c4f544c5a5e2b6d34706a24ec5b4b4
juha.hakala <juha.hakala@49924331-b1e2-4916-88b7-9d944329b4f6>
2011-03-21 08:51:43 +0000

    Added mailer. Note: does not work unless you configure an email server in application.conf.
    git-svn-id: http://dossee.atlantis.fw.sk/droids@66 49924331-b1e2-4916-88b7-9d944329b4f6

---
+++ openarsdd/app/notifiers/Mail.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notifiers;
+
+import play.*;
+import play.mvc.*;
+import java.util.*;
+import models.*;
+
+/**
+ *
+ * @author UserX
+ */
+public class Mail extends Mailer {
+
+    public static void questionCreated(String email, Question createdQuestion) {
+
+        long id = 1;
+        setSubject("Poll created in openars");
+        addRecipient(email);
+        setFrom("noreply@openars.edu");
+        if (createdQuestion != null) {
+            id = createdQuestion.studentLink;
+        }
+
+        send(id);
+    }
+}
